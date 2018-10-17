1cede24bb4b1b9e8eec9b96a0bd2c81aa0f42f44
Christopher Cahill and Devin Sweeting <devin@>
2018-10-17 11:42:04 -0700

    add styles & other functions

--- js/scripts.js
+++ js/scripts.js
@@ -19,14 +19,50 @@ var divide = function(number1, number2) {
 // Everything below this line is user interface (or front-end) logic:
 
 $(document).ready(function(){
-  $("form#add").submit(function(event) {
+  $("form#add-numbers").submit(function(event) {
     event.preventDefault();
+
   var number1 = parseInt($("#add1").val());
   var number2 = parseInt($("#add2").val());
 
   var result = add(number1, number2);
-  
 
-  $("#output").text(result);
+  $("#add-output").text(result);
+  });
+
+  // subtraction
+  $("form#sub-numbers").submit(function(event) {
+    event.preventDefault();
+
+  var number1 = parseInt($("#sub1").val());
+  var number2 = parseInt($("#sub2").val());
+
+  var result = subtract(number1, number2);
+
+  $("#sub-output").text(result);
+  });
+
+  // multiply
+  $("form#mult-numbers").submit(function(event) {
+    event.preventDefault();
+
+  var number1 = parseInt($("#mult1").val());
+  var number2 = parseInt($("#mult2").val());
+
+  var result = multiply(number1, number2);
+
+  $("#mult-output").text(result);
+  });
+
+  // Divide
+  $("form#div-numbers").submit(function(event) {
+    event.preventDefault();
+
+  var number1 = parseInt($("#div1").val());
+  var number2 = parseInt($("#div2").val());
+
+  var result = divide(number1, number2);
+
+  $("#div-output").text(result);
   });
 });
